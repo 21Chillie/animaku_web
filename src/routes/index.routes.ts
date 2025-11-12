504944746585eec9132b5c271323ab7ca39910b9
Chillie <reskialamsyah@gmail.com>
2025-11-13 05:36:17 +0800

    DELETE: rename index.routes.ts to index.route.ts

--- src/routes/index.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import type { Request, Response } from "express";
-
-export const indexRoute = express.Router();
-
-indexRoute.get("/", (req: Request, res: Response) => {
-  res.render("index")
-});
