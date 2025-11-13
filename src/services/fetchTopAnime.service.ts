ac35a6774e1100a7ce973d88d127caa495d11153
Chillie <reskialamsyah@gmail.com>
2025-11-14 06:54:47 +0800

    UPDATE: service for fething top anime data fully function

---
+++ src/services/fetchTopAnime.service.ts
@@ -0,0 +1,54 @@
+import axios, { AxiosResponse } from "axios";
+import dotenv from "dotenv";
+import { Anime, JikanResponse } from "../types/animeData.types";
+
+dotenv.config();
+
+const API_URL = process.env.JIKAN_BASE_URL;
+
+export async function fetchTopAnimeBatch(maxPage: number): Promise<Anime[]> {
+	const topAnimeList: Anime[] = [];
+
+	// Loop request max 4 pages for fetching only 100 anime trending data
+	for (let page = 1; page <= maxPage; page++) {
+		try {
+			// Delay Request
+			if (page > 1) {
+				await new Promise((resolve) => setTimeout(resolve, 200));
+			}
+
+			const response: AxiosResponse<JikanResponse> = await axios.get(`${API_URL}/top/anime`, {
+				params: {
+					page: page,
+				},
+				timeout: 10000,
+			});
+			const topAnimeData: Anime[] = response.data.data;
+
+			// If the data is available and the data is array then push
+			if (topAnimeData && Array.isArray(topAnimeData)) {
+				topAnimeList.push(...topAnimeData);
+
+				console.log(`Page ${page}: ${topAnimeList.length} top anime titles fetched from API`);
+			}
+
+			// Stop early if no more pages available
+			if (!response.data.pagination.has_next_page) {
+				console.log(`⏹️ No more pages available. Stopping at page ${page}`);
+				break;
+			}
+		} catch (err) {
+			if (err instanceof Error) {
+				console.error("Error fetching top anime list from api: ", err);
+			}
+
+			if (axios.isAxiosError(err) && err.code === "ECONNABORTED") {
+				console.error("Request timed out");
+			}
+
+			throw new Error("Something went wrong while fetching topanime list from API!");
+		}
+	}
+
+	return topAnimeList;
+}
