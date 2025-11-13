2b7d592f7919975d0000477d37e37441038237fd
Chillie <reskialamsyah@gmail.com>
2025-11-14 06:49:55 +0800

    UPDATE: models database operation for anime_trending table

---
+++ src/models/animeTrendingModel.ts
@@ -0,0 +1,32 @@
+import pool from "../config/database.config";
+import { QueryResult } from "pg";
+import { DatabaseAnimeTypes } from "../types/databaseAnime.types";
+
+export async function getOldAnimeTrending(daysThreshold: number): Promise<DatabaseAnimeTypes[]> {
+	const result: QueryResult<DatabaseAnimeTypes> = await pool.query(
+		`SELECT * FROM anime_trending WHERE created_at <= NOW() - make_interval(days => $1)`,
+		[daysThreshold]
+	);
+
+	return result.rows;
+}
+
+export async function deleteOldAnimeTrending(daysThreshold: number): Promise<number> {
+	const result = await pool.query(`DELETE FROM anime_trending WHERE created_at <= NOW() - make_interval(days => $1)`, [daysThreshold]);
+
+	return result.rowCount ?? 0;
+}
+
+export async function getAllAnimeTrending(): Promise<DatabaseAnimeTypes[]> {
+	const result: QueryResult<DatabaseAnimeTypes> = await pool.query(
+		`SELECT * FROM anime_trending ORDER BY (data->>'popularity'):: INTEGER ASC`
+	);
+
+	return result.rows;
+}
+
+export async function getAnimeTrendingLimit(maxRecords: number): Promise<DatabaseAnimeTypes[]> {
+	const result = await pool.query(`SELECT * FROM anime_trending ORDER BY (data->>'popularity'):: INTEGER ASC LIMIT $1`, [maxRecords]);
+
+	return result.rows;
+}
