3fdc6d3e6c79c8ec9365201530f1d9857da7ef26
Chillie <reskialamsyah@gmail.com>
2025-11-14 06:50:27 +0800

    DELETE: rename this files

--- src/routes/anime.route.ts
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { getAnimeTrending } from "../controllers/anime.controller";
-
-export const animeRoutes = express.Router();
-
-animeRoutes.get("/seasons/now", getAnimeTrending);
