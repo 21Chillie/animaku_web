f76444c4079f074047ede983c789bdfb28417b61
Chillie <reskialamsyah@gmail.com>
2025-11-14 06:52:58 +0800

    UPDATE: remove unused import and fix indexRoute

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,7 +1,6 @@
 import express from "express";
-import type { Request, Response } from "express";
-import { renderAnimeTrending } from "../controllers/index.controller";
+import { renderDataIndex } from "../controllers/index.controller";
 
 export const indexRoute = express.Router();
 
-indexRoute.get("/", renderAnimeTrending);
+indexRoute.get("/", renderDataIndex);
