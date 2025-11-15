ea445be542c38b78f6b01e361a2121da025a08d6
Chillie <reskialamsyah@gmail.com>
2025-11-15 22:56:12 +0800

    ADD: function for manga top model for database operation

---
+++ src/models/mangaTopModel.ts
@@ -0,0 +1,30 @@
+import pool from "../config/database.config";
+import { QueryResult } from "pg";
+import { DatabaseMangaTypes } from "../types/database.types";
+
+export async function getOldMangaTop(daysThreshold: number): Promise<DatabaseMangaTypes[]> {
+	const result: QueryResult<DatabaseMangaTypes> = await pool.query(
+		`SELECT * FROM manga_top WHERE created_at <= NOW() - make_interval(days => $1)`,
+		[daysThreshold]
+	);
+
+	return result.rows;
+}
+
+export async function deleteOldMangaTop(daysThreshold: number): Promise<number> {
+	const result = await pool.query(`DELETE FROM manga_top WHERE created_at <= NOW() - make_interval(days => $1)`, [daysThreshold]);
+
+	return result.rowCount ?? 0;
+}
+
+export async function getAllMangaTop(): Promise<DatabaseMangaTypes[]> {
+	const result: QueryResult<DatabaseMangaTypes> = await pool.query(`SELECT * FROM manga_top t ORDER BY t.score DESC`);
+
+	return result.rows;
+}
+
+export async function getMangaTopLimit(maxRecords: number): Promise<DatabaseMangaTypes[]> {
+	const result = await pool.query(`SELECT * FROM manga_top t ORDER BY t.score DESC LIMIT $1`, [maxRecords]);
+
+	return result.rows;
+}
