df2d161c159c1a2442b61ae5328a5bc6b92ec4ee
Chillie <reskialamsyah@gmail.com>
2025-11-18 09:27:20 +0800

    UPDATE: fix query logic for conflict data

--- src/models/animeTopSeedTable.ts
+++ src/models/animeTopSeedTable.ts
@@ -1,31 +1,35 @@
-import { QueryResult } from "pg";
-import pool from "../config/database.config";
-import { Anime } from "../types/animeData.types";
+import { QueryResult } from 'pg';
+import pool from '../config/database.config';
+import { Anime } from '../types/animeData.types';
 
 export async function seedTableAnimeTop(topAnimeList: Anime[]): Promise<void> {
 	const client = await pool.connect();
 
 	try {
-		await client.query("BEGIN");
+		await client.query('BEGIN');
 
 		for (const anime of topAnimeList) {
 			await client.query(
 				`
-      INSERT INTO anime_top(mal_id, data)
-      VALUES ($1, $2)
-      ON CONFLICT (mal_id)
-      DO UPDATE SET data = EXCLUDED.data
-      `,
+				INSERT INTO anime_top(mal_id, data)
+				VALUES ($1, $2)
+				ON CONFLICT (mal_id)
+				DO UPDATE SET 
+    				data = EXCLUDED.data,
+    				last_updated_at = CURRENT_TIMESTAMP;
+      	`,
 				[anime.mal_id, anime]
 			);
 		}
-		await client.query("COMMIT");
+		await client.query('COMMIT');
 
-		const checkDatabase: QueryResult<Anime[]> = await client.query(`SELECT * FROM anime_top`);
+		const checkDatabase: QueryResult<Anime[]> = await client.query(`SELECT id FROM anime_top`);
 
-		console.log(`Successfully inserting top anime data list (${checkDatabase.rowCount} / ${topAnimeList.length})`);
+		console.log(
+			`Successfully inserting top anime data list (${checkDatabase.rowCount} / ${topAnimeList.length})`
+		);
 	} catch (err) {
-		await client.query("ROLLBACK");
+		await client.query('ROLLBACK');
 
 		if (err instanceof Error) {
 			console.error("Error while seeding table 'anime_top': ", err.message);
--- src/models/animeTrendingSeedTable.ts
+++ src/models/animeTrendingSeedTable.ts
@@ -1,30 +1,34 @@
-import pool from "../config/database.config";
-import { Anime } from "../types/animeData.types";
+import pool from '../config/database.config';
+import { Anime } from '../types/animeData.types';
 
 export async function seedTableAnimeTrending(trendingAnimeList: Anime[]): Promise<void> {
 	const client = await pool.connect();
 
 	try {
-		await client.query("BEGIN");
+		await client.query('BEGIN');
 
 		for (const anime of trendingAnimeList) {
 			await client.query(
 				`
-      INSERT INTO anime_trending(mal_id, data)
-      VALUES ($1, $2)
-      ON CONFLICT (mal_id)
-      DO UPDATE SET data = EXCLUDED.data
+				INSERT INTO anime_trending(mal_id, data)
+				VALUES ($1, $2)
+				ON CONFLICT (mal_id)
+				DO UPDATE SET 
+    			data = EXCLUDED.data,
+    			last_updated_at = CURRENT_TIMESTAMP;
       `,
 				[anime.mal_id, anime]
 			);
 		}
-		await client.query("COMMIT");
+		await client.query('COMMIT');
 
-		const checkDatabase = await client.query(`SELECT * FROM anime_trending`);
+		const checkDatabase = await client.query(`SELECT id FROM anime_trending`);
 
-		console.log(`Successfully inserting trending anime data list (${checkDatabase.rowCount} / ${trendingAnimeList.length})`);
+		console.log(
+			`Successfully inserting trending anime data list (${checkDatabase.rowCount} / ${trendingAnimeList.length})`
+		);
 	} catch (err) {
-		await client.query("ROLLBACK");
+		await client.query('ROLLBACK');
 
 		if (err instanceof Error) {
 			console.error("Error while seeding table 'anime_trending': ", err.message);
