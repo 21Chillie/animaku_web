fcfdcfa012bda03124f8533e433a17f45cd4b10e
Chillie <reskialamsyah@gmail.com>
2025-11-18 09:37:16 +0800

    UPDATE: fix database types so it match the database column format

--- src/types/database.types.ts
+++ src/types/database.types.ts
@@ -4,23 +4,29 @@ import { Manga } from './mangaData.types';
 export interface DatabaseAnimeTypes {
 	id: number;
 	mal_id: number;
-	data: Anime;
 	title: string;
-	score: number | null;
 	type: string;
 	status: string;
+	year: number | null;
+	score: number | null;
+	rank: number | null;
+	popularity: number | null;
+	data: Anime;
 	created_at: string;
-	updated_at: string;
+	last_updated_at: string;
 }
 
 export interface DatabaseMangaTypes {
 	id: number;
 	mal_id: number;
-	data: Manga;
 	title: string;
-	score: number | null;
 	type: string;
 	status: string;
+	year: number | null;
+	score: number | null;
+	rank: number | null;
+	popularity: number | null;
+	data: Manga;
 	created_at: string;
-	updated_at: string;
+	last_updated_at: string;
 }
