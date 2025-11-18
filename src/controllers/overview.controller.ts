bb6feff97704b0e69d70f2934da03132d43f96ba
Chillie <reskialamsyah@gmail.com>
2025-11-18 09:37:32 +0800

    UPDATE: still working on it

--- src/controllers/overview.controller.ts
+++ src/controllers/overview.controller.ts
@@ -1,5 +1,9 @@
 import type { Request, Response } from 'express';
 
+function getAnimeData (type: string, id: number) {
+
+}
+
 export async function renderOverview(req: Request, res: Response) {
 	res.render('overview');
 }
