cda2b6760c5cd81f60f5b1c5819ac2caf19fbfce
Chillie <reskialamsyah@gmail.com>
2025-11-18 09:26:41 +0800

    UPDATE: adding more columns for each tables

--- src/models/animeTrendingCreateTable.ts
+++ src/models/animeTrendingCreateTable.ts
@@ -1,4 +1,4 @@
-import pool from "../config/database.config";
+import pool from '../config/database.config';
 
 export async function createTableTrendingAnime(): Promise<void> {
 	const client = await pool.connect();
@@ -8,18 +8,25 @@ export async function createTableTrendingAnime(): Promise<void> {
   CREATE TABLE anime_trending (
     id SERIAL PRIMARY KEY,
     mal_id INTEGER UNIQUE NOT NULL,
-    data JSONB NOT NULL,
     title VARCHAR(255) GENERATED ALWAYS AS (data->>'title') STORED,
-    score DECIMAL(3,2) GENERATED ALWAYS AS ((data->>'score')::DECIMAL) STORED,
     type VARCHAR(50) GENERATED ALWAYS AS (data->>'type') STORED,
     status VARCHAR(50) GENERATED ALWAYS AS (data->>'status') STORED,
+    year INTEGER GENERATED ALWAYS AS ((data->'aired'->'prop'->'from'->>'year')::INTEGER) STORED,
+    score DECIMAL(3,2) GENERATED ALWAYS AS ((data->>'score')::DECIMAL) STORED,
+    rank INTEGER GENERATED ALWAYS AS ((data->>'rank')::INTEGER) STORED,
+    popularity INTEGER GENERATED ALWAYS AS ((data->>'popularity')::INTEGER) STORED,
+    data JSONB NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    last_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
     CREATE INDEX idx_anime_trending_mal_id ON anime_trending(mal_id);
+    CREATE INDEX idx_anime_trending_title ON anime_trending(title);
     CREATE INDEX idx_anime_trending_score ON anime_trending(score);
+    CREATE INDEX idx_anime_trending_rank ON anime_trending(rank);
+    CREATE INDEX idx_anime_trending_popularity ON anime_trending(popularity);
     CREATE INDEX idx_anime_trending_type ON anime_trending(type);
+    CREATE INDEX idx_anime_trending_year ON anime_trending(year);
     CREATE INDEX idx_anime_trending_data ON anime_trending USING GIN (data);
   `
 	);
@@ -28,5 +35,3 @@ export async function createTableTrendingAnime(): Promise<void> {
 
 	client.release();
 }
-
-createTableTrendingAnime();
