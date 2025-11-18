7e7a6786b55fea9561fabd873f6bc46233b8f066
Chillie <reskialamsyah@gmail.com>
2025-11-18 09:24:56 +0800

    UPDATE: fix models for manga top table operation

--- src/models/mangaTopModel.ts
+++ src/models/mangaTopModel.ts
@@ -1,10 +1,10 @@
-import pool from "../config/database.config";
-import { QueryResult } from "pg";
-import { DatabaseMangaTypes } from "../types/database.types";
+import { QueryResult } from 'pg';
+import pool from '../config/database.config';
+import { DatabaseMangaTypes } from '../types/database.types';
 
 export async function getOldMangaTop(daysThreshold: number): Promise<DatabaseMangaTypes[]> {
 	const result: QueryResult<DatabaseMangaTypes> = await pool.query(
-		`SELECT * FROM manga_top WHERE created_at <= NOW() - make_interval(days => $1)`,
+		`SELECT * FROM manga_top WHERE last_updated_at <= NOW() - make_interval(days => $1)`,
 		[daysThreshold]
 	);
 
@@ -12,19 +12,26 @@ export async function getOldMangaTop(daysThreshold: number): Promise<DatabaseMan
 }
 
 export async function deleteOldMangaTop(daysThreshold: number): Promise<number> {
-	const result = await pool.query(`DELETE FROM manga_top WHERE created_at <= NOW() - make_interval(days => $1)`, [daysThreshold]);
+	const result = await pool.query(
+		`DELETE FROM manga_top WHERE last_updated_at <= NOW() - make_interval(days => $1)`,
+		[daysThreshold]
+	);
 
 	return result.rowCount ?? 0;
 }
 
 export async function getAllMangaTop(): Promise<DatabaseMangaTypes[]> {
-	const result: QueryResult<DatabaseMangaTypes> = await pool.query(`SELECT * FROM manga_top t ORDER BY t.score DESC`);
+	const result: QueryResult<DatabaseMangaTypes> = await pool.query(
+		`SELECT * FROM manga_top ORDER BY score DESC`
+	);
 
 	return result.rows;
 }
 
 export async function getMangaTopLimit(maxRecords: number): Promise<DatabaseMangaTypes[]> {
-	const result = await pool.query(`SELECT * FROM manga_top t ORDER BY t.score DESC LIMIT $1`, [maxRecords]);
+	const result = await pool.query(`SELECT * FROM manga_top t ORDER BY t.score DESC LIMIT $1`, [
+		maxRecords,
+	]);
 
 	return result.rows;
 }
