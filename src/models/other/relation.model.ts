20654026f226eb42efd2d6c61b11c76f967255cf
Chillie <reskialamsyah@gmail.com>
2025-11-19 10:51:09 +0800

    ADD: database operation for relation table

---
+++ src/models/other/relation.model.ts
@@ -0,0 +1,30 @@
+import { QueryResult } from 'pg';
+import pool from '../../config/database.config';
+import { DatabaseRelationResponse, Relation } from '../../types/relationData.types';
+
+export async function insertAnimeRelationByMalId(mal_id: number, data: Relation[]) {
+	try {
+		await pool.query(
+			`
+			INSERT INTO anime_relation(mal_id, relation_data)
+			VALUES ($1, $2)
+			ON CONFLICT (mal_id)
+			DO UPDATE SET 
+    		relation_data = EXCLUDED.relation_data,
+    		last_updated_at = CURRENT_TIMESTAMP;
+			`,
+			[mal_id, data]
+		);
+	} catch (err) {
+		console.error('Error while inserting anime relation data into database: ', err);
+	}
+}
+
+export async function getAnimeRelationByMalId(mal_id: number) {
+	const result: QueryResult<DatabaseRelationResponse> = await pool.query(
+		`SELECT relation_data, created_at, last_updated_at FROM anime_relation WHERE mal_id = $1`,
+		[mal_id]
+	);
+
+	return result.rows[0];
+}
