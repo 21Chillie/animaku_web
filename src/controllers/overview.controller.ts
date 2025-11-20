9c6487c62f4e0dc9ad69e96b3c70a87e07727a18
Chillie <reskialamsyah@gmail.com>
2025-11-20 12:39:44 +0800

    UPDATE: finish getMangaData function and all the data will render in overview page (almost finish, just need to add opening/ending OST)

--- src/controllers/overview.controller.ts
+++ src/controllers/overview.controller.ts
@@ -2,23 +2,34 @@ import axios from 'axios';
 import type { Request, Response } from 'express';
 import { JIKAN_BASE_URL } from '../config/env.config';
 import { getAnimeByMalId, insertAnimeDataByMalId } from '../models/anime/animeDBModel';
+import { getMangaByMalId, insertMangaDataByMalId } from '../models/manga/mangaDB.model';
 import {
 	getAnimeCharactersByMalId,
+	getMangaCharactersByMalId,
 	insertAnimeCharacterByMalId,
+	insertMangaCharacterByMalId,
 } from '../models/other/character.model';
 import {
 	getAnimeRecommendationByMalId,
+	getMangaRecommendationByMalId,
 	insertAnimeRecommendationByMalId,
+	insertMangaRecommendationByMalId,
 } from '../models/other/recommendation.model';
 import {
 	getAnimeRelationByMalId,
+	getMangaRelationByMalId,
 	insertAnimeRelationByMalId,
+	insertMangaRelationByMalId,
 } from '../models/other/relation.model';
 import {
 	fetchAnimeByMalId,
 	fetchAnimeCharactersByMalId,
 	fetchAnimeRecommendationByMalId,
 	fetchAnimeRelationByMalId,
+	fetchMangaByMalId,
+	fetchMangaCharactersByMalId,
+	fetchMangaRecommendationByMalId,
+	fetchMangaRelationByMalId,
 } from '../services/fetchTitleData';
 
 const API_URL = JIKAN_BASE_URL;
@@ -34,6 +45,17 @@ async function getAnimeData(mal_id: number) {
 			await insertAnimeDataByMalId(dataFromAPI);
 			animeData = await getAnimeByMalId(mal_id);
 		}
+
+		function formatSynopsis(synopsis: string | null) {
+			if (!synopsis) return '';
+
+			return synopsis
+				.replace(/\\"/g, '"') // Unescape quotes
+				.replace(/\n\n/g, '</p><p class="written-by text-sm mb-4">') // Paragraphs for EJS
+				.replace(/\n/g, '<br>'); // Single line breaks
+		}
+
+		const synopsis = formatSynopsis(animeData.data.synopsis);
 
 		// 2. Get or fetch character data
 		let animeCharacter = await getAnimeCharactersByMalId(mal_id);
@@ -97,6 +119,7 @@ async function getAnimeData(mal_id: number) {
 
 		return {
 			anime: animeData,
+			synopsis: synopsis,
 			character: animeCharacter,
 			relation: {
 				prequel: relPrequel,
@@ -113,7 +136,101 @@ async function getAnimeData(mal_id: number) {
 }
 
 async function getMangaData(mal_id: number) {
-	// TODO: Do the same as getAnimeData()
+	try {
+		// 1. Get or fetch manga data
+		let mangaData = await getMangaByMalId(mal_id);
+
+		if (!mangaData) {
+			console.log('Run fetch manga');
+			const dataFromAPI = await fetchMangaByMalId(mal_id);
+			await insertMangaDataByMalId(dataFromAPI);
+			mangaData = await getMangaByMalId(mal_id);
+		}
+
+		function formatSynopsis(synopsis: string | null) {
+			if (!synopsis) return '';
+
+			return synopsis
+				.replace(/\\"/g, '"') // Unescape quotes
+				.replace(/\n\n/g, '</p><p class="written-by text-sm mb-4">') // Paragraphs for EJS
+				.replace(/\n/g, '<br>'); // Single line breaks
+		}
+
+		const synopsis = formatSynopsis(mangaData.data.synopsis);
+
+		// 2. Get or fetch manga character data
+		let mangaCharacter = await getMangaCharactersByMalId(mal_id);
+
+		if (!mangaCharacter) {
+			console.log('Run fetch manga character');
+			const dataFromAPI = await fetchMangaCharactersByMalId(mal_id);
+			await insertMangaCharacterByMalId(mal_id, dataFromAPI);
+			mangaCharacter = await getMangaCharactersByMalId(mal_id);
+		}
+
+		if (mangaCharacter.character_data.length <= 6) {
+			console.log('Not enough character, re run fetch manga character');
+			const dataFromAPI = await fetchMangaCharactersByMalId(mal_id);
+			await insertMangaCharacterByMalId(mal_id, dataFromAPI);
+			mangaCharacter = await getMangaCharactersByMalId(mal_id);
+		}
+
+		// 3 Get or fetch manga relation data
+		let mangaRelation = await getMangaRelationByMalId(mal_id);
+
+		if (!mangaRelation) {
+			console.log('Run fetch anime relation');
+			const dataFromAPI = await fetchMangaRelationByMalId(mal_id);
+			await insertMangaRelationByMalId(mal_id, dataFromAPI);
+			mangaRelation = await getMangaRelationByMalId(mal_id);
+		}
+
+		const relPrequel = mangaRelation.relation_data.find((rel) => rel.relation === 'Prequel');
+		const relSequel = mangaRelation.relation_data.find((rel) => rel.relation === 'Sequel');
+		const relAdaptation = mangaRelation.relation_data.find((rel) => rel.relation === 'Adaptation');
+		const relSideStory = mangaRelation.relation_data.find((rel) => rel.relation === 'Side Story');
+
+		// 4. Get or fetch manga recommendation data
+		let mangaRecommendation = await getMangaRecommendationByMalId(mal_id);
+
+		if (!mangaRecommendation) {
+			console.log('Run fetch manga recommendation');
+			const dataFromAPI = await fetchMangaRecommendationByMalId(mal_id);
+			await insertMangaRecommendationByMalId(mal_id, dataFromAPI);
+			mangaRecommendation = await getMangaRecommendationByMalId(mal_id);
+		}
+
+		if (mangaRecommendation.recommendation_data.length <= 6) {
+			console.log('Not enough recommendation, re run fetch manga recommendation');
+			const dataFromAPI = await fetchMangaRecommendationByMalId(mal_id);
+			await insertMangaRecommendationByMalId(mal_id, dataFromAPI);
+			mangaRecommendation = await getMangaRecommendationByMalId(mal_id);
+
+			if (mangaRecommendation.recommendation_data.length <= 6) {
+				console.log('Still not enough recommendation, fetch random manga recommendation');
+				await new Promise((resolve) => setTimeout(resolve, 1000));
+				const response = await axios.get(`${API_URL}/manga/24705/recommendations`);
+				const data = response.data.data.slice(0, 10);
+				mangaRecommendation = data;
+			}
+		}
+
+		return {
+			manga: mangaData,
+			synopsis: synopsis,
+			character: mangaCharacter,
+			relation: {
+				prequel: relPrequel,
+				sequel: relSequel,
+				adaptation: relAdaptation,
+				side_story: relSideStory,
+			},
+			recommendation: mangaRecommendation,
+		};
+	} catch (err) {
+		console.error('Error in getAnimeData:', err);
+		throw err; // Re-throw to let caller handle it
+	}
 }
 
 export async function renderOverview(req: Request, res: Response) {
@@ -121,6 +238,14 @@ export async function renderOverview(req: Request, res: Response) {
 	const type = req.params.type;
 
 	if (type === 'anime') {
-		return res.status(200).json(await getAnimeData(id));
+		const data = await getAnimeData(id);
+		// return res.status(200).json({ type, data });
+		return res.status(200).render('overview', { type, data });
+	}
+
+	if (type === 'manga') {
+		const data = await getMangaData(id);
+		// return res.status(200).json({ type, data });
+		return res.status(200).render('overview', { type, data });
 	}
 }
