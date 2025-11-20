f76d33cef46201fb24670af926f850c0a8dacc53
Chillie <reskialamsyah@gmail.com>
2025-11-20 12:36:10 +0800

    UPDATE: add function for database operation manga relation table

--- src/models/other/relation.model.ts
+++ src/models/other/relation.model.ts
@@ -28,3 +28,30 @@ export async function getAnimeRelationByMalId(mal_id: number): Promise<DatabaseR
 
 	return result.rows[0];
 }
+
+export async function insertMangaRelationByMalId(mal_id: number, data: Relation[]) {
+	try {
+		await pool.query(
+			`
+			INSERT INTO manga_relation(mal_id, relation_data)
+			VALUES ($1, $2)
+			ON CONFLICT (mal_id)
+			DO UPDATE SET 
+    		relation_data = EXCLUDED.relation_data,
+    		last_updated_at = CURRENT_TIMESTAMP;
+			`,
+			[mal_id, data]
+		);
+	} catch (err) {
+		console.error('Error while inserting manga relation data into database: ', err);
+	}
+}
+
+export async function getMangaRelationByMalId(mal_id: number): Promise<DatabaseRelationResponse> {
+	const result: QueryResult<DatabaseRelationResponse> = await pool.query(
+		`SELECT relation_data FROM manga_relation WHERE mal_id = $1`,
+		[mal_id]
+	);
+
+	return result.rows[0];
+}
