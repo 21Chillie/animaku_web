c9056f90d7d350623aa8aecd9e10c4e2a185b898
Chillie <reskialamsyah@gmail.com>
2025-11-20 12:35:46 +0800

    UPDATE: add function for database operation for manga recommendation table

--- src/models/other/recommendation.model.ts
+++ src/models/other/recommendation.model.ts
@@ -33,3 +33,32 @@ export async function getAnimeRecommendationByMalId(
 
 	return result.rows[0];
 }
+
+export async function insertMangaRecommendationByMalId(mal_id: number, data: Recommendation[]) {
+	try {
+		await pool.query(
+			`
+			INSERT INTO manga_recommendation(mal_id, recommendation_data)
+			VALUES ($1, $2)
+			ON CONFLICT (mal_id)
+			DO UPDATE SET 
+    		recommendation_data = EXCLUDED.recommendation_data,
+    		last_updated_at = CURRENT_TIMESTAMP;
+			`,
+			[mal_id, data]
+		);
+	} catch (err) {
+		console.error('Error while inserting manga recommendation data into database: ', err);
+	}
+}
+
+export async function getMangaRecommendationByMalId(
+	mal_id: number
+): Promise<DatabaseRecommendationResponse> {
+	const result: QueryResult<DatabaseRecommendationResponse> = await pool.query(
+		`SELECT recommendation_data FROM manga_recommendation WHERE mal_id = $1`,
+		[mal_id]
+	);
+
+	return result.rows[0];
+}
