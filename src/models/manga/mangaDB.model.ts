42fd6a070cab466e989835d305bc66e27e72b4da
Chillie <reskialamsyah@gmail.com>
2025-11-20 12:37:34 +0800

    ADD: function for database operation manga table

---
+++ src/models/manga/mangaDB.model.ts
@@ -0,0 +1,31 @@
+import type { QueryResult } from 'pg';
+import pool from '../../config/database.config';
+import { DatabaseMangaTypes } from '../../types/database.types';
+import { Manga } from '../../types/mangaData.types';
+
+export async function insertMangaDataByMalId(data: Manga) {
+	try {
+		await pool.query(
+			`
+			INSERT INTO manga(mal_id, data)
+			VALUES ($1, $2)
+			ON CONFLICT (mal_id)
+			DO UPDATE SET 
+    		data = EXCLUDED.data,
+    		last_updated_at = CURRENT_TIMESTAMP;
+			`,
+			[data.mal_id, data]
+		);
+	} catch (err) {
+		console.error('Error while inserting manga data into database: ', err);
+	}
+}
+
+export async function getMangaByMalId(mal_id: number): Promise<DatabaseMangaTypes> {
+	const result: QueryResult<DatabaseMangaTypes> = await pool.query(
+		`SELECT * FROM manga WHERE mal_id = $1`,
+		[mal_id]
+	);
+
+	return result.rows[0];
+}
