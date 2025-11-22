50305515820bc7621335333dd6a29ec2af9a0b61
Chillie <reskialamsyah@gmail.com>
2025-11-22 15:29:53 +0800

    DELETE: remove both apiManga and apiAnime routes

--- src/routes/apiManga.route.ts
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { getMangaTop } from "../controllers/apiGetMangaTop.controller";
-
-export const mangaRoutes = express.Router();
-
-mangaRoutes.get("/top", getMangaTop);
