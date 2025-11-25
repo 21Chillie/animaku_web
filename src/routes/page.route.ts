b61600b21df304b02bb9b9c44b3d7e716765c986
Chillie <reskialamsyah@gmail.com>
2025-11-25 11:49:25 +0800

    UPDATE: fix import modules and add trending and top title list (manga/anime) route

--- src/routes/page.route.ts
+++ src/routes/page.route.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-import { renderIndex } from '../controllers/index.controller';
-import { renderOverview } from '../controllers/overview.controller';
+import { renderIndex } from '../controllers/page/index.controller';
+import { renderOverview } from '../controllers/page/overview.controller';
+import { top100Anime } from '../controllers/page/list.controrller';
 
 export const pageRoute = express.Router();
 
@@ -9,3 +10,6 @@ pageRoute.get('/', renderIndex);
 
 // Overview route
 pageRoute.get('/overview/:type/:id', renderOverview);
+
+// Trending and Top Title List
+pageRoute.get('/list/:type/:category', top100Anime);
