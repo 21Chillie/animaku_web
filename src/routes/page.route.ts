6d25a241180ecf9348e171d6e6acd4756d81171b
Chillie <reskialamsyah@gmail.com>
2025-11-27 10:10:23 +0800

    UPDATE: add page route for showing character page

--- src/routes/page.route.ts
+++ src/routes/page.route.ts
@@ -1,12 +1,16 @@
 import express from 'express';
+import { renderCharacter } from '../controllers/page/character.controller';
 import { renderIndex } from '../controllers/page/index.controller';
-import { renderOverview } from '../controllers/page/overview.controller';
 import { top100Anime } from '../controllers/page/list.controrller';
+import { renderOverview } from '../controllers/page/overview.controller';
 
 export const pageRoute = express.Router();
 
 // Index route
 pageRoute.get('/', renderIndex);
+
+// Overview Character
+pageRoute.get('/overview/character/:id', renderCharacter);
 
 // Overview route
 pageRoute.get('/overview/:type/:id', renderOverview);
