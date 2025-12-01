dee51ed0a7583482e6a4e9f171b592047846bdf5
Chillie <reskialamsyah@gmail.com>
2025-12-01 21:36:18 +0800

    ADD: function for fetch anime/manga data from api backend

---
+++ public/js/browseService.js
@@ -0,0 +1,7 @@
+const searchInput = document.getElementById('search-filter');
+const mediaFilter = document.getElementById('media-filter');
+const typeFilter = document.getElementById('type-filter');
+const statusFilter = document.getElementById('status-filter');
+const sortByFilter = document.getElementById('sort-by-filter');
+const sortDirectionFilter = document.getElementById('sort-direction-filter');
+const listContainer = document.getElementById('list-container');
