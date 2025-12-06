51453f632ec36768ed63fd776363cf619ddf9816
Chillie <reskialamsyah@gmail.com>
2025-12-06 18:01:37 +0800

    UPDATE: add routes for login and sign up page

--- src/routes/page.route.ts
+++ src/routes/page.route.ts
@@ -1,4 +1,10 @@
 import express from 'express';
+import {
+	postLogin,
+	postRegister,
+	renderLogin,
+	renderRegister,
+} from '../controllers/page/authPage.route';
 import { renderBrowse } from '../controllers/page/browse.route';
 import { renderCharacter } from '../controllers/page/character.controller';
 import { renderIndex } from '../controllers/page/index.controller';
@@ -9,6 +15,14 @@ export const pageRoute = express.Router();
 
 // Index route
 pageRoute.get('/', renderIndex);
+
+// Login Route
+pageRoute.get('/login', renderLogin);
+pageRoute.post('/login', postLogin);
+
+// Sign Up Route
+pageRoute.get('/register', renderRegister);
+pageRoute.post('/register', postRegister);
 
 // Overview Character
 pageRoute.get('/overview/character/:id', renderCharacter);
