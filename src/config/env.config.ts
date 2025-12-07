58cfb2b9373daaa8f91490dfd9b52a6dcfb8c085
Chillie <reskialamsyah@gmail.com>
2025-12-08 02:08:59 +0800

    UPDATE: add environment variables for google and session

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,5 +1,19 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 dotenv.config({ quiet: true });
 
-export const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD, JIKAN_BASE_URL } = process.env;
+export const {
+	PORT,
+	NODE_ENV,
+	SESSION_SECRET,
+	CSRF_SECRET,
+	DB_HOST,
+	DB_PORT,
+	DB_NAME,
+	DB_USER,
+	DB_PASSWORD,
+	JIKAN_BASE_URL,
+	GOOGLE_CLIENT_ID,
+	GOOGLE_CLIENT_SECRET,
+	GOOGLE_CALLBACK_URL,
+} = process.env;
