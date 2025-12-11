8a20943693b13030e1145ea0dc348303948efd34
Chillie <reskialamsyah@gmail.com>
2025-12-11 23:43:28 +0800

    UPDATE: refactor all the function and fix the error handling

--- src/models/anime/animeTopModel.ts
+++ src/models/anime/animeTopModel.ts
@@ -1,44 +1,105 @@
 import { QueryResult } from 'pg';
 import pool from '../../config/database.config';
 import { DatabaseAnimeTypes } from '../../types/database.types';
+import { DatabaseError } from '../../utils/databaseError.utils';
 
 export async function getOldAnimeTop(daysThreshold: number): Promise<DatabaseAnimeTypes[]> {
-	const result: QueryResult<DatabaseAnimeTypes> = await pool.query(
-		`SELECT * FROM anime_top WHERE last_updated_at <= NOW() - make_interval(days => $1)`,
-		[daysThreshold]
-	);
+	const client = await pool.connect();
 
-	return result.rows;
+	try {
+		const result: QueryResult<DatabaseAnimeTypes> = await client.query(
+			`SELECT * FROM anime_top WHERE last_updated_at <= NOW() - make_interval(days => $1)`,
+			[daysThreshold]
+		);
+
+		return result.rows;
+	} catch (err: unknown) {
+		console.error(`Error getting old records (>${daysThreshold} days) from 'anime_top':`, err);
+		throw new DatabaseError(`Failed to get old records from 'anime_top' table`, { cause: err });
+	} finally {
+		client.release();
+	}
 }
 
-export async function deleteOldAnimeTop(daysThreshold: number): Promise<number> {
-	const result = await pool.query(
-		`DELETE FROM anime_top WHERE last_updated_at <= NOW() - make_interval(days => $1)`,
-		[daysThreshold]
-	);
+export async function deleteOldAnimeTop(daysThreshold: number) {
+	const client = await pool.connect();
 
-	return result.rowCount ?? 0;
+	try {
+		await client.query(
+			`DELETE FROM anime_top WHERE last_updated_at <= NOW() - make_interval(days => $1)`,
+			[daysThreshold]
+		);
+	} catch (err: unknown) {
+		console.error(`Error deleting records (>${daysThreshold} days) from 'anime_top':`, err);
+		throw new DatabaseError(`Failed to delete old records from 'anime_top' table`, { cause: err });
+	} finally {
+		client.release();
+	}
+}
+
+export async function deleteAllOldAnimeTop() {
+	const client = await pool.connect();
+
+	try {
+		await client.query(`TRUNCATE TABLE anime_top RESTART IDENTITY;`);
+	} catch (err: unknown) {
+		console.error(`Error truncating 'anime_top' table:`, err);
+		throw new DatabaseError(`Failed to delete all records from 'anime_top' table`, { cause: err });
+	} finally {
+		client.release();
+	}
 }
 
 export async function getAllAnimeTop(): Promise<DatabaseAnimeTypes[]> {
-	const result: QueryResult<DatabaseAnimeTypes> = await pool.query(
-		`SELECT * FROM anime_top t ORDER BY t.score DESC`
-	);
+	const client = await pool.connect();
 
-	return result.rows;
+	try {
+		const result: QueryResult<DatabaseAnimeTypes> = await client.query(
+			`SELECT * FROM anime_top t ORDER BY t.score DESC`
+		);
+
+		return result.rows;
+	} catch (err: unknown) {
+		console.error(`Error getting all records from 'anime_top' table:`, err);
+		throw new DatabaseError(`Failed to get all records from 'anime_top' table`, { cause: err });
+	} finally {
+		client.release();
+	}
 }
 
 export async function getAnimeTopLimit(maxRecords: number): Promise<DatabaseAnimeTypes[]> {
-	const result = await pool.query(`SELECT * FROM anime_top t ORDER BY t.score DESC LIMIT $1`, [
-		maxRecords,
-	]);
+	const client = await pool.connect();
 
-	return result.rows;
+	try {
+		const result = await client.query(`SELECT * FROM anime_top t ORDER BY t.score DESC LIMIT $1`, [
+			maxRecords,
+		]);
+
+		return result.rows;
+	} catch (err: unknown) {
+		console.error(`Error getting ${maxRecords} records from 'anime_top' table:`, err);
+		throw new DatabaseError(`Failed to get ${maxRecords} records from 'anime_top' table`, {
+			cause: err,
+		});
+	} finally {
+		client.release();
+	}
 }
 
 export async function getAnimeTopCount(): Promise<number> {
-	const result = await pool.query(`SELECT COUNT(*) FROM anime_top`);
-	return parseInt(result.rows[0].count);
+	const client = await pool.connect();
+
+	try {
+		const result = await client.query(`SELECT * FROM anime_top`);
+		return result.rowCount ?? 0;
+	} catch (err: unknown) {
+		console.error(`Error getting total record count from 'anime_top' table:`, err);
+		throw new DatabaseError(`Failed to get total record count from 'anime_top' table`, {
+			cause: err,
+		});
+	} finally {
+		client.release();
+	}
 }
 
 export async function getAnimeTopPaginated(
@@ -55,6 +116,12 @@ export async function getAnimeTopPaginated(
 			[limit, offset]
 		);
 		return result.rows;
+	} catch (err: unknown) {
+		console.error(
+			`Error getting paginated data from 'anime_top' table (limit=${limit}, offset=${offset}):`,
+			err
+		);
+		throw new DatabaseError(`Failed to get paginated data from 'anime_top' table`, { cause: err });
 	} finally {
 		client.release();
 	}
--- src/models/anime/animeTrendingModel.ts
+++ src/models/anime/animeTrendingModel.ts
@@ -1,45 +1,111 @@
 import { QueryResult } from 'pg';
 import pool from '../../config/database.config';
 import { DatabaseAnimeTypes } from '../../types/database.types';
+import { DatabaseError } from '../../utils/databaseError.utils';
 
 export async function getOldAnimeTrending(daysThreshold: number): Promise<DatabaseAnimeTypes[]> {
-	const result: QueryResult<DatabaseAnimeTypes> = await pool.query(
-		`SELECT * FROM anime_trending WHERE last_updated_at <= NOW() - make_interval(days => $1)`,
-		[daysThreshold]
-	);
+	const client = await pool.connect();
 
-	return result.rows;
+	try {
+		const result: QueryResult<DatabaseAnimeTypes> = await client.query(
+			`SELECT * FROM anime_trending WHERE last_updated_at <= NOW() - make_interval(days => $1)`,
+			[daysThreshold]
+		);
+
+		return result.rows;
+	} catch (err: unknown) {
+		console.error(`Error getting old records (>${daysThreshold} days) from 'anime_trending':`, err);
+		throw new DatabaseError(`Failed to get old records from 'anime_trending' table`, {
+			cause: err,
+		});
+	} finally {
+		client.release();
+	}
 }
 
-export async function deleteOldAnimeTrending(daysThreshold: number): Promise<number> {
-	const result = await pool.query(
-		`DELETE FROM anime_trending WHERE last_updated_at <= NOW() - make_interval(days => $1)`,
-		[daysThreshold]
-	);
+export async function deleteOldAnimeTrending(daysThreshold: number) {
+	const client = await pool.connect();
 
-	return result.rowCount ?? 0;
+	try {
+		await client.query(
+			`DELETE FROM anime_trending WHERE last_updated_at <= NOW() - make_interval(days => $1)`,
+			[daysThreshold]
+		);
+	} catch (err: unknown) {
+		console.error(`Error deleting records (>${daysThreshold} days) from 'anime_trending':`, err);
+		throw new DatabaseError(`Failed to delete old records from 'anime_trending' table`, {
+			cause: err,
+		});
+	} finally {
+		client.release();
+	}
+}
+
+export async function deleteAllOldAnimeTrending() {
+	const client = await pool.connect();
+	try {
+		await client.query(`TRUNCATE TABLE anime_trending RESTART IDENTITY;`);
+	} catch (err: unknown) {
+		console.error("Error truncating 'anime_trending' table:", err);
+		throw new DatabaseError(`Failed to delete all records from 'anime_trending' table`, {
+			cause: err,
+		});
+	} finally {
+		client.release();
+	}
 }
 
 export async function getAllAnimeTrending(): Promise<DatabaseAnimeTypes[]> {
-	const result: QueryResult<DatabaseAnimeTypes> = await pool.query(
-		`SELECT * FROM anime_trending ORDER BY (data->>'popularity'):: INTEGER ASC`
-	);
+	const client = await pool.connect();
+	try {
+		const result: QueryResult<DatabaseAnimeTypes> = await client.query(
+			`SELECT * FROM anime_trending ORDER BY (data->>'popularity'):: INTEGER ASC`
+		);
 
-	return result.rows;
+		return result.rows;
+	} catch (err: unknown) {
+		console.error(`Error getting all records from 'anime_trending' table:`, err);
+		throw new DatabaseError(`Failed to get all records from 'anime_trending' table`, {
+			cause: err,
+		});
+	} finally {
+		client.release();
+	}
 }
 
 export async function getAnimeTrendingLimit(maxRecords: number): Promise<DatabaseAnimeTypes[]> {
-	const result = await pool.query(
-		`SELECT * FROM anime_trending ORDER BY (data->>'popularity'):: INTEGER ASC LIMIT $1`,
-		[maxRecords]
-	);
+	const client = await pool.connect();
+	try {
+		const result = await client.query(
+			`SELECT * FROM anime_trending ORDER BY (data->>'popularity'):: INTEGER ASC LIMIT $1`,
+			[maxRecords]
+		);
 
-	return result.rows;
+		return result.rows;
+	} catch (err: unknown) {
+		console.error(`Error getting ${maxRecords} records from 'anime_trending' table:`, err);
+		throw new DatabaseError(`Failed to get ${maxRecords} records from 'anime_trending' table`, {
+			cause: err,
+		});
+	} finally {
+		client.release();
+	}
 }
 
 export async function getAnimeTrendingCount(): Promise<number> {
-	const result = await pool.query(`SELECT COUNT(*) FROM anime_trending`);
-	return parseInt(result.rows[0].count);
+	const client = await pool.connect();
+
+	try {
+		const result = await client.query(`SELECT * FROM anime_trending`);
+		return result.rowCount ?? 0;
+	} catch (err: unknown) {
+		console.error(`Error getting total record count from 'anime_trending' table:`, err);
+		throw new DatabaseError(`Failed to get total record count from 'anime_trending' table`, {
+			cause: err,
+		});
+	} finally {
+		client.release();
+	}
 }
 
 export async function getAnimeTrendingPaginated(
@@ -56,6 +122,14 @@ export async function getAnimeTrendingPaginated(
 			[limit, offset]
 		);
 		return result.rows;
+	} catch (err: unknown) {
+		console.error(
+			`Error getting paginated data from 'anime_trending' table (limit=${limit}, offset=${offset}):`,
+			err
+		);
+		throw new DatabaseError(`Failed to get paginated data from 'anime_trending' table`, {
+			cause: err,
+		});
 	} finally {
 		client.release();
 	}
