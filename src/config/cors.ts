80a0bbbf33f05059cc8e13738519e3c5cfd0fd18
Chillie <reskialamsyah@gmail.com>
2026-01-08 03:34:41 +0800

    ADD: cors config

---
+++ src/config/cors.ts
@@ -0,0 +1,24 @@
+import { PORT } from "./env.config";
+
+// Allowed origins
+const allowedOrigins = [
+	"https://animaku.chillie.dpdns.org", // Cloudflare tunnel
+	`http://localhost:${PORT}`, // Local dev
+];
+
+// CORS options
+export const corsOptions = {
+	origin: function (
+		origin: string | undefined,
+		callback: (err: Error | null, allow?: boolean) => void
+	) {
+		// Allow requests with no origin (like mobile apps, curl, or server-to-server requests)
+		if (!origin) return callback(null, true);
+		if (allowedOrigins.indexOf(origin) !== -1) {
+			callback(null, true); // allowed
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+	credentials: true, // if you need cookies/auth
+};
